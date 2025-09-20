9197b77a239330fe8b9836a18987ce9eac09187e
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-19 21:45:10 -0500

    json2obj todo data

---
+++ source/TodoData.hx
@@ -0,0 +1,36 @@
+package;
+
+import Checkbox.CheckboxStates;
+import json2object.JsonParser;
+import lime.utils.Assets;
+
+class TodoData
+{
+	@:default([])
+	public var entry_names:Array<String> = [];
+
+	@:default([])
+	public var entry_values:Array<CheckboxStates> = [];
+
+	@:jignored
+	public var id:String = '';
+
+	public function new(id:String)
+	{
+		var parser = new JsonParser<TodoData>();
+		var json = parser.fromJson(Assets.getText('assets/lists/' + id + '.json'));
+
+		if (json == null)
+			throw 'Error parsing TODO list: ' + id;
+
+		this.id = id;
+
+		this.entry_names = json.entry_names;
+		this.entry_values = json.entry_values;
+	}
+
+	public function toString():String
+	{
+		return 'TodoData(id:' + id + ', entry_names: ' + entry_names + ', entry_values: ' + entry_values + ')';
+	}
+}
